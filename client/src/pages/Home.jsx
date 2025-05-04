9b14110280be39374d7737d0959ca00b684877ed
Dinesh Shrestha <87228800+shresthadinesh@users.noreply.github.com>
2025-05-04 14:09:52 +0545

    Project

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Mainbanner from "../components/MainBanner";
+import Categories from "../components/Categories";
+import BestSeller from "../components/BestSeller";
+import BottomBanner from "../components/BottomBanner";
+import NewsLetter from "../components/NewsLetter";
+
+const Home = () => {
+  return (
+    <div className="mt-10">
+      <Mainbanner />
+      <Categories />
+      < BestSeller />
+      < BottomBanner />
+      < NewsLetter />
+    </div>
+  );
+};
+
+export default Home;
